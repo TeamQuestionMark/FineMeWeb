7a2c238ba6d00445f52e6cfec294bcf6dce4768f
HaYeong <hy78733@gmail.com>
2023-05-20 02:37:09 +0900

    setting: eslint prettier, remove testing code

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['react-app', 'plugin:prettier/recommended'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'prettier/prettier': 'warn',
+  },
+};
