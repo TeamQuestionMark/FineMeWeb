a239229cacc55b54b8a3f890cb51370e359035bf
HaYeong <hy78733@gmail.com>
2023-06-27 14:27:34 +0900

    feat: 라우터

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <RouterProvider router={router} />,
 );
