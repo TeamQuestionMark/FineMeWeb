24370f09b0e435bfa63d80f981202cce040524d9
HaYeong <hy78733@gmail.com>
2023-07-30 13:05:45 +0900

    feat: ResultRenderer 컴포넌트, 더미 데이터로 렌더링

---
+++ src/pages/Result/ResultRenderer.tsx
@@ -0,0 +1,37 @@
+import { LoaderData } from '@/router/types';
+import { useMemo } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { dummyQuestions } from '../Stage/dummy';
+import { dummyAnswerResults } from './dummy';
+import renderResult from './renderResult';
+
+interface ResultRendererProps {}
+const ResultRenderer: React.FC<ResultRendererProps> = () => {
+  // const {
+  //   questionData: {
+  //     stageQuestionPage: { content: questions },
+  //   },
+  //   answerData: { stageQuestionAnswerList: answers },
+  // } = useLoaderData() as LoaderData['ResultPage'];
+  const questions = dummyQuestions;
+  const answers = dummyAnswerResults;
+  const results = useMemo(() => {
+    return answers.map(answer => {
+      const matchedQuestion = questions.find(
+        q => q.questionId === answer.questionId,
+      );
+      return { question: matchedQuestion, answer };
+    });
+  }, [answers, questions]);
+
+  return (
+    <>
+      {results.map(({ question, answer }) => {
+        if (question) return renderResult(question, answer);
+        return <></>;
+      })}
+    </>
+  );
+};
+
+export default ResultRenderer;
